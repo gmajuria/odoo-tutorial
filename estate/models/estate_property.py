5f4861df784f913401ccbf21b00c8b2084da9dc8
gmajuria <gmajuria@gmail.com>
2024-10-14 17:21:19 -0300

    Add model estate property tag

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -24,12 +24,19 @@ class EstateProperty(models.Model):
     active = fields.Boolean(string='Active', default=True)
     state = fields.Selection([('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')], string='State', required=True, copy=False, default='new')
     property_type_id = fields.Many2one('estate.property.type', string='Property Type')
-    partner_id = fields.Many2one('res.partner', string='Buyer')
+    partner_id = fields.Many2one('res.partner', string='Buyer', copy=False)
     user_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
-
+    property_tag_ids = fields.Many2many('estate.property.tag', string='Property Tags')
 
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Property Type'
+
+    name = fields.Char(string='Name', required=True)
+
+
+class EstatePropertyTag(models.Model):
+    _name = 'estate.property.tag'
+    _description = 'Property Tag'
 
     name = fields.Char(string='Name', required=True)
